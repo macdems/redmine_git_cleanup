dbfd447c3925de233e45d297a320cfc8bb20dfee
Maciej Dems <maciej.dems@p.lodz.pl>
2018-08-26 18:54:07 +0200

    Added multi-threading

--- lib/repository_git_patch.rb
+++ lib/repository_git_patch.rb
@@ -10,27 +10,28 @@ module RepositoryGitPatch
   end
 
   module InstanceMethods
-
     def fetch_changesets_with_cleanup
       fetch_changesets_without_cleanup
-
-      cl = changesets.map { |c| c if extra_info['heads'].include? c.revision }.compact
-      cs = changesets.to_set - cl.to_set
-      cl.each do |c|
-        c.parents.each do |p|
-          if cs.include? p
-            cl.append p
-            cs.delete p
+      Thread.new do
+        ActiveRecord::Base.connection_pool.with_connection do
+          cl = changesets.map { |c| c if extra_info['heads'].include? c.revision }.compact
+          cs = changesets.to_set - cl.to_set
+          cl.each do |c|
+            c.parents.each do |p|
+              if cs.include? p
+                cl.append p
+                cs.delete p
+              end
+            end
           end
+          cs.each do |c|
+            c.destroy
+            Rails.logger.info "Deleted orphaned revision #{c.revision} from #{project.identifier}/#{identifier}"
+          end
+          save
         end
       end
-      cs.each do |c|
-        c.destroy
-        Rails.logger.info "Deleted orphaned revision #{c.revision} from #{project.identifier}/#{identifier}"
-      end
-      save
     end
-
   end
 
 end
